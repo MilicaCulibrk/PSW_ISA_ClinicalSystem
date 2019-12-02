4a1ef8a4e0d73d97e70d949daf5858d81b1115a6
AnaNikolasevic <23nikolasevic@gmail.com>
2019-12-02 16:42:43 +0100

    task odradjen 1.12.2019.

--- client/src/components/pocetnaMedicinskeSestre.vue
+++ client/src/components/pocetnaMedicinskeSestre.vue
@@ -1,157 +1,106 @@
 <template>
-
-
-
-    
     <div>
-
-            
-
-
-            <div id="viewport">
-                    <!-- Sidebar -->
-
-
-
-                    
-                    <div id="sidebar">
-                      <header>
-                        <a href="#">.</a>
-                      </header>
-                      <ul class="nav">
-                            
-                                    <a href="#">
-                                      <i v-on:click="otvoriFormu()" >           
-                                   </i>
-                                   
-                                   
-                                   PROFIL   
-                                  
-                                   
-                                  
-                                
-                                  </li>
-                                </a>
-                          <a href="#">
-                            <i class="zmdi zmdi-view-dashboard"></i>
-
-
-                               KALENDAR
-                          </a>
-                        </li>
-                       
-                          <a href="#">
-                            <i class="zmdi zmdi-link"></i> 
-
-
-                            LISTA PACIJENATA
-                          </a>
-                        </li>
-                       
-                        
-                       
-                
-                       
-                      </ul>
+	    <div id="viewport">
+	        <!-- Sidebar -->       
+	        <div id="sidebar">
+	          <header>
+	            <a href="#">.</a>
+	          </header>
+	          <ul class="nav">
+				  <a href="#">
+					<i v-on:click="otvoriFormu" > PROFIL            
+					</i>                                  				     
+				  </a>
+	              <a href="#">
+	                <i class="zmdi zmdi-view-dashboard"> KALENDAR
+	                </i>                   
+	              </a>
+	              <a href="#">
+	                <i class="zmdi zmdi-view-dashboard"> LISTA KLINIKA
+	                </i>                   
+	              </a>	               
+	           </ul>
+	        </div>
+            <!-- Content -->
+            <div id="content">                    
+              <div class="container-fluid">
+                <h1 style="color: #b3b3b3;">Medicinska Sestra - {{ korisnik.ime }} {{ korisnik.prezime }}</h1>                
+              </div>
+            </div>
+	     </div>
+	
+      <form   class="message-form" style="position: relative; top: 10px; left: 400px; width: 800px; height: 620px; background-color: rgba(130, 206, 209, 0.733); ">
+        <div>	                    
+          <div  class="container d-flex justify-content-center" style="margin-top: 30px">	                        
+            <div class="card" style="width: 99.5%; height: 99.5%; margin-top: 5px; margin-bottom: 5px">	                    
+              <div class="form-group">
+        
+                <div class="card-body mx-4 mt-4">
+                  <div class="row">
+        
+                    <div class="col">
+                    <div class="md-form">
+                      <label for="Form-username" style="color: #b3b3b3;">E-mail</label>
+                      <input type="text" id="Form-username" class="form-control" v-model=korisnik.email disabled>
+                      
+                      <label for="Form-ime" style="color: #b3b3b3;">Ime</label>
+                      <input type="text" id="Form-ime" class="form-control" v-model=korisnik.ime :disabled="!izmeni">
+                      
+                      <label for="Form-phone" style="color: #b3b3b3;">Telefon</label>
+                      <input type="text" id="Form-phone" class="form-control" v-model=korisnik.telefon :disabled="!izmeni">
+                      
+                      <label for="Form-email4" style="color: #b3b3b3;">Adresa</label>
+                      <input type="text" id="Form-email4" class="form-control" v-model=korisnik.adresa :disabled="!izmeni">
+                      
+                      <label for="Form-email4" style="color: #b3b3b3;">JMBG</label>
+                      <input type="text" id="Form-email4" class="form-control" v-model=korisnik.jmbg disabled>
+        
                     </div>
-                    <!-- Content -->
-                    <div id="content">
-                            
-                      <div class="container-fluid">
-                        <h1 style="color: #b3b3b3;">Medicinska Sestra</h1>
-                        
-                      </div>
+                    </div>
+                    <div class="col">
+                    <div class="md-form pb-3">
+        
+                      <label for="Form-city" style="color: #b3b3b3;">Lozinka</label>
+                      <input type="text" id="Form-city" class="form-control" v-model=korisnik.lozinka disabled>
+                      
+                      <label for="Form-prezime" style="color: #b3b3b3;">Prezime</label>
+                      <input type="text" id="Form-prezime" class="form-control" v-model=korisnik.prezime :disabled="!izmeni">
+        
+                      
+                      <label for="Form-city" style="color: #b3b3b3;">Grad</label>
+                      <input type="text" id="Form-city" class="form-control" v-model=korisnik.grad :disabled="!izmeni">
+        
+                  
+                      <label for="Form-city" style="color: #b3b3b3;">Drzava</label>
+                      <input type="text" id="Form-city" class="form-control" v-model=korisnik.drzava :disabled="!izmeni">
+                     
+                      <label for="Form-city" style="color: #b3b3b3;">Ocene</label>
+                      <input type="text" id="Form-ocene" class="form-control" v-model=korisnik.ocene :disabled="!izmeni">
+                    </div>
                     </div>
                   </div>
-
-
-
-                  <form   class="message-form" style="position: relative; top: 10px; left: 400px; width: 800px; height: 620px; background-color: rgba(130, 206, 209, 0.733); ">
-                        <div>
-                                
-                            
-                              <div  class="container d-flex justify-content-center" style="margin-top: 30px">
-                            
-                                
-                                <div class="card" style="width: 99.5%; height: 99.5%; margin-top: 5px; margin-bottom: 5px">
-                            
-                                 
-                                  
-                            
-                            
-                            
-                                  <div class="form-group">
-                            
-                                    <div class="card-body mx-4 mt-4">
-                                      <div class="row">
-                            
-                                        <div class="col">
-                                        <div class="md-form">
-                                          <label for="Form-username" style="color: #b3b3b3;">E-mail</label>
-                                          <input type="text" id="Form-username" class="form-control">
-                                          
-                                          <label for="Form-ime" style="color: #b3b3b3;">Ime</label>
-                                          <input type="text" id="Form-ime" class="form-control" >
-                                          
-                                          <label for="Form-phone" style="color: #b3b3b3;">Telefon</label>
-                                          <input type="text" id="Form-phone" class="form-control">
-                                          
-                                          <label for="Form-email4" style="color: #b3b3b3;">Adresa</label>
-                                          <input type="text" id="Form-email4" class="form-control" >
-                                          
-                                          <label for="Form-email4" style="color: #b3b3b3;">JMBG</label>
-                                          <input type="text" id="Form-email4" class="form-control">
-                                        
-                                          
-                            
-                                        </div>
-                                        </div>
-                                        <div class="col">
-                                        <div class="md-form pb-3">
-                            
-                                          <label for="Form-city" style="color: #b3b3b3;">Lozinka</label>
-                                          <input type="text" id="Form-city" class="form-control">
-                                          
-                                          <label for="Form-prezime" style="color: #b3b3b3;">Prezime</label>
-                                          <input type="text" id="Form-prezime" class="form-control" >
-                            
-                                          
-                                          <label for="Form-city" style="color: #b3b3b3;">Grad</label>
-                                          <input type="text" id="Form-city" class="form-control">
-                            
-                                      
-                                          <label for="Form-city" style="color: #b3b3b3;">Drzava</label>
-                                          <input type="text" id="Form-city" class="form-control" >
-                                         
-                                          <label for="Form-city" style="color: #b3b3b3;">Ocene</label>
-                                          <input type="text" id="Form-ocene" class="form-control" >
-                                        </div>
-                                        </div>
-                                      </div>
-                                      
-                            
-                            
-                                      <div class="text-center mb-4 mt-4">
-                                        <template>
-                                        <button type="button" class="btn btn-danger btn-block z-depth-2" style=" color: #37474F; width: 100px; height: 35px;border-color: rgba(130, 206, 209, 0.733); ; background-color: rgba(130, 206, 209, 0.733); " >Izmeni</button>
-                                        </template>
-                                        <template>
-                                        <button type="button" class="btn btn-success btn-block z-depth-2"  style=" color: #37474F; width: 100px; height: 35px;border-color: rgba(130, 206, 209, 0.733); ; background-color: rgba(130, 206, 209, 0.733); " >Sačuvaj</button>
-                                        <button type="button" class="btn btn-danger btn-block z-depth-2"  style=" color: #37474F; width: 100px; height: 35px;border-color: rgba(130, 206, 209, 0.733); ; background-color: rgba(130, 206, 209, 0.733); " >Odustani</button>
-                                        </template>
-                                      </div>
-                            
-                                    </div>
-                            
-                                  </div>
-                            
-                            
-                                </div>
-                                
-                            
-                              </div>
-                              </div>
-                </form>
+        
+                  <div class="text-center mb-4 mt-4">
+                    <template>
+                    <button type="button" v-if="!izmeni" v-on:click="izmena" class="btn btn-danger btn-block z-depth-2" style=" color: #37474F; width: 100px; height: 35px;border-color: rgba(130, 206, 209, 0.733); ; background-color: rgba(130, 206, 209, 0.733); " >Izmeni</button>
+                    </template>
+                    <template>
+                    <button type="button" v-if="izmeni" v-on:click="sacuvaj" class="btn btn-success btn-block z-depth-2"  style=" color: #37474F; width: 100px; height: 35px;border-color: rgba(130, 206, 209, 0.733); ; background-color: rgba(130, 206, 209, 0.733); " >Sačuvaj</button>
+                    <button type="button" v-if="izmeni" v-on:click="odustani" class="btn btn-danger btn-block z-depth-2"  style=" color: #37474F; width: 100px; height: 35px;border-color: rgba(130, 206, 209, 0.733); ; background-color: rgba(130, 206, 209, 0.733); " >Odustani</button>
+                    </template>
+                  </div>
+        
+                </div>
+        
+              </div>
+        
+        
+            </div>
+            
+        
+          </div>
+          </div>
+        </form>
 
                   
 </div>
@@ -163,15 +112,66 @@
 </template>
 
 <script>
-    export default {
- data() {
-    prikaz = false;
-  },
-  methods: {
-        otvoriFormu(){
-            this.prikaz=!this.prikaz;
-        }
-    }
+import axios from 'axios'
+export default {
+	data() {
+		return{
+			korisnik: {},
+			prikaz: false,
+			izmeni: false,
+		}
+	},
+	methods: {
+		otvoriFormu(){
+			this.prikaz=!this.prikaz
+		},
+		izmena(){
+			this.izmeni = true
+		},
+		odustani(){
+	        this.izmeni = false
+	        axios
+	        .get("http://localhost:8081/medicinskaSestra/get")
+	        .then(medicinskaSestra =>{
+		          this.korisnik = medicinskaSestra.data;
+		      })
+		    .catch(error => {
+		          console.log(error)
+		    });			
+		},
+		sacuvaj(){
+			if(this.korisnik.ime === "" || this.korisnik.prezime === "" || this.korisnik.adresa === "" || this.korisnik.grad === "" || this.korisnik.drzava === ""
+            	|| this.korisnik.telefon === "") 
+            	{
+                    alert("Polja ne smeju biti prazna!");
+                    return;
+				}
+            var rex = /^\+381\/6[0-9]-?[0-9]+(-[0-9]+)?$/;
+            if (!rex.test(String(this.korisnik.telefon.trim()))) {
+            	alert("Broj telefona treba da bude oblika +381/65-504205");    
+             	return;
+            }
+            axios
+            .put("http://localhost:8081/medicinskaSestra/izmeni", this.korisnik)
+            .then(medicinskaSestra =>{
+              this.korisnik = medicinskaSestra.data;
+              this.izmeni = false;
+            })
+            .catch(error => {
+                console.log(error)
+            });
+	  	}
+	},
+	mounted() {
+      axios
+      .get('http://localhost:8081/medicinskaSestra/get')
+      .then(medicinskaSestra =>{
+        this.korisnik = medicinskaSestra.data;
+      })
+      .catch(error => {
+          console.log(error)
+      });
+  	}
 };
 
 </script>
