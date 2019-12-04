a1e7fe70d15a8b5b9206269bfcbd700dd81931b5
stanislavaskoric <milica6.1.97thebest@gmail.com>
2019-12-04 18:27:04 +0100

    Registracija pacijenta

---
+++ server/src/main/java/main/service/PacijentService.java
@@ -0,0 +1,20 @@
+package main.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import main.model.Pacijent;
+import main.repository.PacijentRepository;
+
+@Service
+public class PacijentService {
+	
+
+	@Autowired
+	private PacijentRepository pacijentRepository;
+	
+	public Pacijent findByEmail(String mail) {
+		return pacijentRepository.findByEmail(mail);
+	}
+
+}
